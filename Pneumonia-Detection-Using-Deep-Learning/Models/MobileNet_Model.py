fa7f80d423d7bda45154fbd158ec74dd5e9ce948
Saurabh <saurabhsingh8602@gmail.com>
2025-10-25 23:45:34 +0530

    Added MobileNet code

---
+++ Pneumonia-Detection-Using-Deep-Learning/Models/MobileNet_Model.py
@@ -0,0 +1,6 @@
+class MobileNetModel:
+    def __init__(self):
+        self.model = None  # Load later in Milestone_2
+
+    def predict(self, img):
+        return "Normal (MobileNet Placeholder)"
