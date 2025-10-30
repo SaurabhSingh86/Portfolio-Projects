f81c25bbddbd9803d5f29778eebc986c3cb775ca
Saurabh <saurabhsingh8602@gmail.com>
2025-10-30 18:06:50 +0530

    Added Home Page

--- Car-Detection-Using-Deep-Learning/UI/Home_Page.py
+++ Car-Detection-Using-Deep-Learning/UI/Home_Page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
 import textwrap
 import sys, cv2
 import pydicom
